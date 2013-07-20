d46405a6ef05c19a8758545cca31139c946d7b41
Bryan te Beek <bryantebeek@gmail.com>
2013-07-21 00:38:29 +0200

    Make meta aware contract only require one function for forward compatibility.

--- src/Calotype/SEO/Contracts/MetaAware.php
+++ src/Calotype/SEO/Contracts/MetaAware.php
@@ -2,7 +2,17 @@
 
 interface MetaAware
 {
-    public function getMetaTitle();
-    public function getMetaTitleSuffix();
-    public function getMetaDescription();
+    /**
+     * Get the data for the meta fields.
+     *
+     * <code>
+     * $data = array(
+     *     'title' => 'About us',
+     *     'description' => 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'
+     * );
+     * </code>
+     *
+     * @return array
+     */
+    public function getMetaData();
 }
